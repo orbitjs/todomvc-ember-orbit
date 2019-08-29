74ab455d8d2e3480f5a566801a96a1f66c8ddebe
Dan Gebhardt <dan@cerebris.com>
2019-08-29 17:35:49 -0400

    Make application route compatible with a backup source

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -7,6 +7,14 @@ export default class extends Route {
   @service dataCoordinator;
 
   async beforeModel() {
+    // If a backup source is present, populate the store from backup prior to
+    // activating the coordinator
+    const backup = this.dataCoordinator.getSource("backup");
+    if (backup) {
+      const transform = await backup.pull(q => q.findRecords());
+      await this.store.sync(transform);
+    }
+
     await this.dataCoordinator.activate();
     await this.store.query(q => q.findRecords("todo"));
   }
