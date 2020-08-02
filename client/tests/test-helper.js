9d468bd59b75e586032c788c0318d9050a0a31ff
Dan Gebhardt <dan@cerebris.com>
2020-08-02 15:10:37 -0400

    Update ember and ember-cli to v3.20

--- client/tests/test-helper.js
+++ client/tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'todomvc-ember-orbit-client/app';
+import config from 'todomvc-ember-orbit-client/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
