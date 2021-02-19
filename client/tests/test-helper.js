db0a2dd44fb6a140cb0bcd7b082ce5ad3be21fd5
Dan Gebhardt <dan@cerebris.com>
2021-02-19 16:08:45 -0500

    Update ember-cli v3.20.0...v3.25.0
    # Conflicts:
    #	client/.eslintignore
    #	client/.gitignore
    #	client/.template-lintrc.js
    #	client/app/router.js
    #	client/app/templates/application.hbs

--- client/tests/test-helper.js
+++ client/tests/test-helper.js
@@ -1,8 +1,12 @@
 import Application from 'todomvc-ember-orbit-client/app';
 import config from 'todomvc-ember-orbit-client/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
