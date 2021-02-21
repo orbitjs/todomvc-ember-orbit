f81964e84c568fa4aca65644c980f496d517b6f7
Dan Gebhardt <dan@cerebris.com>
2021-02-21 12:55:38 -0500

    Fix tests/index.html and add a simple acceptance test

---
+++ tests/acceptance/home-test.js
@@ -0,0 +1,16 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { waitForSource } from 'ember-orbit/test-support';
+
+module('Acceptance | home', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function (assert) {
+    await visit('/');
+
+    await waitForSource('store');
+
+    assert.equal(currentURL(), '/');
+  });
+});
